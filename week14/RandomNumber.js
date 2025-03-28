bdb2f7bfd0c51959b3da5c1ac2fc2660f3a6470e
Harini-win <harini.selvakummar@gmail.com>
2025-03-28 10:25:42 +0530

    till week 14

---
+++ week14/RandomNumber.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+const RandomNumber = () => {
+    const [randomNumber, setRandomNumber] = useState(null);
+
+    const generateRandomNumber = () => {
+        const num = Math.floor(Math.random() * 100) + 1;
+        setRandomNumber(num);
+    };
+
+    return (
+        <div>
+            <button onClick={generateRandomNumber}>Generate Random Number</button>
+            {randomNumber !== null && <p>Random Number: {randomNumber}</p>}
+        </div>
+    );
+};
+
+export default RandomNumber;
